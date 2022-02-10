85810918170139245b4317c7d916b4e1e520ad53
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-02-10 08:06:38 +0000

    {Update}

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
     if @book.save
      redirect_to book_path(@book), notice: 'Book was successfully created'
     else
-      
+      @books = Book.all
+      render :index
     end
   end
 
@@ -23,15 +24,18 @@ class BooksController < ApplicationController
   end
 
   def update
-      book = Book.find(params[:id])
-      book.update(book_params)
-      redirect_to book_path(book.id), notice: 'Book was successfully updated'
+      @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
       book = Book.find(params[:id])
       book.destroy
-      redirect_to '/books'
+      redirect_to books_path
   end
 
   private
